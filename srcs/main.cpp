ebcdb8678af3e8c6f6106c8dd1a7aa63ffc12119
Remy GENY <rgeny@e2r2p16.clusters.42paris.fr>
2022-03-31 18:09:22 +0200

    merge to rgeny_makefile

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 14:00:06 by rgeny             #+#    #+#             */
-/*   Updated: 2022/03/31 14:48:19 by abesombe         ###   ########.fr       */
+/*   Updated: 2022/03/31 18:09:07 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	main	(int		argc
 			,char **	argv)
 {
 	std::vector<std::string>	arg	(argv + 1, argv + argc);
-
 
 	return (0);
 }
