303ee66feaeb9c75706473a8fb2ef290c1f782d3
rgeny <remy.geny@gmail.com>
2022-04-18 22:18:34 +0200

    add inheritate Socket in Client and message queue in Client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 12:43:21 by rgeny             #+#    #+#             */
-/*   Updated: 2022/04/18 21:56:15 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/04/18 22:14:08 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 # define DFL_NICKNAME "anonymous"
 
 # include <iostream>
+# include <queue>
 # include "Socket.hpp"
 # include "Utils.hpp"
 
 class Client
+	:public Socket
 {
 	public:
 		Client	(void);
@@ -31,9 +33,14 @@ class Client
 		const std::string &	get_nickname	(void) const;
 		void				set_nickname	(std::string nickname);
 
+		void	add_to_queue		(std::string & msg);
+		bool	is_empty_msg_queue	(void) const;
+		int		send				(void);
+
 	private:
-		Socket		_socket;
-		std::string	_nickname;
+//		Socket					_socket;
+		std::string				_nickname;
+		std::queue<std::string>	_msg_queue;
 };
 
 std::ostream &	operator<<	(std::ostream & os
--- srcs/Client/Client.member.cpp
+++ srcs/Client/Client.member.cpp
@@ -6,9 +6,32 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 14:57:53 by rgeny             #+#    #+#             */
-/*   Updated: 2022/04/12 14:57:53 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/04/18 22:16:36 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 
+void	Client::add_to_queue	(std::string & msg)
+{
+	this->_msg_queue.push(msg);
+}
+
+bool	Client::is_empty_msg_queue	(void) const
+{
+	return (this->_msg_queue.empty());
+}
+
+int		Client::send	(void)
+{
+	if (this->_msg_queue.empty())
+		std::cout	<< "Queue is empty, clients can't send a msg."
+					<< std::endl;
+	else
+	{
+		std::string &	msg = this->_msg_queue.front();
+		this->_msg_queue.pop();
+		return (this->Socket::send(msg));
+	}
+	return (-1);
+}
