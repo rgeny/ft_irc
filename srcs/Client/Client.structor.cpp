e8cd0c0cd75dc8782174a4309302ad2fd9dd4a62
rgeny <remy.geny@gmail.com>
2022-04-23 17:46:13 +0200

    add be_disconnect member in Client class

--- srcs/Client/Client.structor.cpp
+++ srcs/Client/Client.structor.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 12:45:06 by rgeny             #+#    #+#             */
-/*   Updated: 2022/04/23 15:19:01 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/04/23 17:45:47 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ Client:: Client	(void)
 	:_nickname(DFL_NICKNAME)
 	,_msg_timer(time(NULL))
 	,_passwd_is_sent(false)
+	,be_disconnected(false)
 {
 	std::cout	<< "Client dfl constructor."
 				<< std::endl;
@@ -25,6 +26,7 @@ Client:: Client	(std::string nickname)
 	:_nickname(nickname)
 	,_msg_timer(time(NULL))
 	,_passwd_is_sent(false)
+	,be_disconnected(false)
 {
 	std::cout	<< "Client parameter constructor."
 				<< std::endl;
@@ -33,7 +35,8 @@ Client:: Client	(std::string nickname)
 Client:: Client	(Client & src)
 	:_nickname(src.get_nickname())
 	,_msg_timer(time(NULL))
-	,_passwd_is_sent(false)
+	,_passwd_is_sent(src._passwd_is_sent)
+	,be_disconnected(src.be_disconnected)
 {
 	std::cout	<< "Client cpy constructor."
 				<< std::endl;
