2f42d2ab1da7b58f2e9beab06a0a5d0f77a79078
rgeny <remy.geny@gmail.com>
2022-04-23 16:36:17 +0200

    del commentary

--- srcs/Command/Command_function/nick.cpp
+++ srcs/Command/Command_function/nick.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 04:31:25 by rgeny             #+#    #+#             */
-/*   Updated: 2022/04/23 16:20:17 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/04/23 16:36:10 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,43 +50,4 @@ int		Command::_nick	(std::vector<std::string> & cmd)
 		this->_client->add_to_queue(final_msg);
 	this->_data._historical.new_node(oldest_nickname, cmd[1]);
 	return (0);
-
-
-
-
-
-
-
-
-
-
-
-
-
-//	if (cmd.size() <= 1)
-//	{
-//		this->_reply.add_arg(cmd[0]);
-//		std::string final_msg = this->_reply.forge(ERR_NONICKNAMEGIVEN);
-//		this->_client->add_to_queue(final_msg);
-//		return (-1);
-//	}
-//	else
-//	{
-//		this->_reply.add_arg(cmd[1]);
-//		std::cout << "nickname: " << cmd[1] << std::endl;
-//		if (cmd.size() > 1)
-//		{
-//			if (this->_client->set_nickname(cmd[1]) == false)
-//			{
-//				std::string final_msg = this->_reply.forge(ERR_ERRONEUSNICKNAME);
-//				std::cout	<< "msg forge : " 
-//							<< final_msg << "\n";
-//				this->_client->add_to_queue(final_msg);
-//				return (-1);
-//			}
-//			std::cout << "NICKNAME SET: " << this->_client->get_nickname() << std::endl;
-//		}
-//		return (0);
-//	}
-//	return (-1);
 }
