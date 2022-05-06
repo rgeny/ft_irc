7178fa0dd45ed10351c09545d46bd777c4c7912f
Remy GENY <rgeny@e3r2p16.clusters.42paris.fr>
2022-05-06 19:22:16 +0200

    modif user cmd

--- srcs/Command/Command_function/user.cpp
+++ srcs/Command/Command_function/user.cpp
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 04:31:51 by rgeny             #+#    #+#             */
-/*   Updated: 2022/05/06 18:11:46 by abesombe         ###   ########.fr       */
+/*   Updated: 2022/05/10 14:21:46 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 
 e_error		Command::_user	(void)
 {
-	// if ((*this->_users_it)->get_nickname() != "anonymous")
-	// {
-		if (this->_cmd.size() > 4)
-		{
-			User & cur_user = *(*this->_users_it);
+	if (this->_cmd.size() > 4)
+	{
+		User & cur_user = *(*this->_users_it);
 
-			cur_user.set_username(this->_cmd[1]);
-		
-			//tmp
-			String	tmp;
-			size_t	size = this->_cmd.size();
-			for (size_t i = 4; i < size; i++)
-				tmp += this->_cmd[i];
-			//fin tmp
-			std::cout	<< "realname : "
-						<< tmp
-						<< "\n";
-			cur_user.set_realname(tmp);
-		}
-		return (SUCCESS);
-	// }
-	return (ERROR_CONTINUE);
+		cur_user.set_username(this->_cmd[1]);
+	
+		//tmp
+		String	tmp;
+		size_t	size = this->_cmd.size();
+		for (size_t i = 4; i < size; i++)
+			tmp += this->_cmd[i];
+		//fin tmp
+		std::cout	<< "realname : "
+					<< tmp
+					<< "\n";
+		cur_user.set_realname(tmp);
+	}
+	return (SUCCESS);
 }
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ayzapata <ayzapata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 11:23:38 by ayzapata          #+#    #+#             */
-/*   Updated: 2022/05/06 19:00:29 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/05/06 19:18:06 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	test	(__attribute((unused)) T s)
 {
 	T	t(av[ac - 1]);
 
-	std::cout	<< s.find('b', 0)
+	std::cout	<< s.find_last_of('b', 4)
 				<< std::endl;
 }
 
@@ -35,47 +35,47 @@ void	test	(__attribute((unused)) T s)
 int	main	(__attribute((unused)) int argc
 			,__attribute((unused)) char ** argv)
 {
-	ac = argc;
-	av = argv;
-
-	String			a(VALUE);
-	std::string		b(VALUE);
-
-	test(a);
-	test(b);
-
-
-//	try
-//	{
-//		int	port;
-//		String	password;
+//	ac = argc;
+//	av = argv;
 //
-//		check_nbr_of_arguments(argc);
-//		check_port_argument(String(argv[1]));
-//		check_password_argument(String(argv[2]));
-//		print_server_name();
-//		port = atoi(argv[1]);
-//		password = argv[2];
+//	String			a(VALUE);
+//	std::string		b(VALUE);
 //
-//		Server	serv (password, port);
-//
-//		serv.main();
-//	}
-//	catch (const wrong_number_of_arguments & e)
-//	{
-//		return (print_error(e.what()));
-//	}
-//	catch (const wrong_port_argument & e)
-//	{
-//		return (print_error(e.what()));
-//	}
-//	catch (const wrong_password_argument & e)
-//	{
-//		return (print_error(e.what()));
-//	}
-//	catch (const error_opening_socket & e)
-//	{
-//		return (print_error(e.what()));
-//	}
+//	test(a);
+//	test(b);
+
+
+	try
+	{
+		int	port;
+		String	password;
+
+		check_nbr_of_arguments(argc);
+		check_port_argument(String(argv[1]));
+		check_password_argument(String(argv[2]));
+		print_server_name();
+		port = atoi(argv[1]);
+		password = argv[2];
+
+		Server	serv (password, port);
+
+		serv.main();
+	}
+	catch (const wrong_number_of_arguments & e)
+	{
+		return (print_error(e.what()));
+	}
+	catch (const wrong_port_argument & e)
+	{
+		return (print_error(e.what()));
+	}
+	catch (const wrong_password_argument & e)
+	{
+		return (print_error(e.what()));
+	}
+	catch (const error_opening_socket & e)
+	{
+		return (print_error(e.what()));
+	}
 	return (EXIT_SUCCESS);
 }
