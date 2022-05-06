61992bb02225c5480457aae193976f7d6daf2e88
Remy GENY <rgeny@e3r2p16.clusters.42paris.fr>
2022-05-06 18:41:34 +0200

    modif compare in String

--- srcs/String/String.compare.cpp
+++ srcs/String/String.compare.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 01:41:40 by rgeny             #+#    #+#             */
-/*   Updated: 2022/04/27 14:53:09 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/05/06 18:38:54 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,14 @@
 
 int		String::compare	(const String & str) const
 {
-	size_t	str_size	= str.size();
-	size_t	this_size	= this->size();
-	size_t	min_size	= std::min(str_size, this_size);
-
-	for (size_t i = 0; i < min_size; i++)
-	{
-		if (this->_cast((*this)[i]) != this->_cast(str[i]))
-			return (this->_cast((*this)[i]) - this->_cast(str[i]));
-	}
-	return (this_size - str_size);
+	return (this->compare(0, this->size(), str, 0, str.size()));
 }
 
 int		String::compare	(size_t pos
 						,size_t len
 						,const String & str) const
 {
-	if (pos > this->size())
-		return (this->std::string::compare(pos, len, str));
-
-	len = std::min(len + pos, this->size());
-
-	size_t	str_size	= str.size();
-
-	for (size_t i = pos, j = 0; i < len && j < str_size; i++, j++)
-	{
-		if (this->_cast((*this)[i]) != this->_cast(str[j]))
-			return (this->_cast((*this)[i]) - this->_cast(str[j]));
-	}
-
-	return ((len - pos) - str_size);
+	return (this->compare(pos, len, str, 0, str.size()));
 }
 
 int	String::compare	(size_t pos
@@ -52,12 +30,6 @@ int	String::compare	(size_t pos
 					,size_t subpos
 					,size_t sublen) const
 {
-	if (pos > this->size()
-		|| subpos > str.size())
-	{
-		return (this->std::string::compare(pos, len, str, subpos, sublen));
-	}
-
 	len = std::min(len + pos, this->size());
 	sublen = std::min(sublen + subpos, str.size());
 
@@ -72,37 +44,17 @@ int	String::compare	(size_t pos
 
 int		String::compare	(const char * s) const
 {
-	size_t	s_size		= strlen(s);
-	size_t	this_size	= this->size();
-	size_t	size_min	= std::min(s_size, this_size);
-
-	for (size_t i = 0; i < size_min; i++)
-	{
-		if (this->_cast((*this)[i]) != this->_cast(s[i]))
-			return (this->_cast((*this)[i]) - this->_cast(s[i]));
-	}
-
-	return (this_size - s_size);
+	String	tmp(s);
+	return (this->compare(0, this->size(), tmp, 0, tmp.size()));
 }
 
 int		String::compare	(size_t pos
 						,size_t len
 						,const char * s) const
 {
-	if (pos > this->size())
-		return (this->std::string::compare(pos, len, s));
+	String	tmp(s);
+	return (this->compare(pos, len, tmp, 0, tmp.size()));
 
-	len = std::min(len + pos, this->size());
-
-	size_t	s_size	= strlen(s);
-
-	for (size_t i = pos, j = 0; i < len && j < s_size; i++, j++)
-	{
-		if (this->_cast((*this)[i]) != this->_cast(s[j]))
-			return (this->_cast((*this)[i]) - this->_cast(s[j]));
-	}
-
-	return ((len - pos) - s_size);
 }
 
 int	String::compare	(size_t pos
@@ -110,19 +62,6 @@ int	String::compare	(size_t pos
 					,const char * s
 					,size_t n) const
 {
-	if (pos > this->size()
-		|| n > strlen(s))
-	{
-		return (this->std::string::compare(pos, len, s, n));
-	}
-
-	len = std::min(len + pos, this->size());
-
-	for (size_t i = pos, j = 0; i < len && j < n; i++, j++)
-	{
-		if (this->_cast((*this)[i]) != this->_cast(s[j]))
-			return (this->_cast((*this)[i]) - this->_cast(s[j]));
-	}
-
-	return ((len - pos) - n);
+	String	tmp(s);
+	return (this->compare(pos, len, tmp, 0, n));
 }
