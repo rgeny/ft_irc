12e05401f0fa99e6aaf052f295a16b33bf1486b0
Remy GENY <rgeny@e3r2p10.clusters.42paris.fr>
2022-05-11 16:27:31 +0200

    cmd quit end

--- srcs/Message/cmd_msg/cmd_error.cpp
+++ srcs/Message/cmd_msg/cmd_error.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:49:15 by rgeny             #+#    #+#             */
-/*   Updated: 2022/05/05 10:13:25 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/05/11 16:26:25 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,16 @@ e_error	Message::_cmd_error	(e_error code)
 	MsgError_it	it = this->_msg_error.find(code);
 	if (it == this->_msg_error.end())
 		return (code);
-	msg += (*it).second;
+	if (code == ERROR_CLIENT_EXITED
+		&& this->_cmd.size() == 2
+		&& !this->_cmd[1].empty())
+	{
+		msg	+= "[Quit: "
+			+ this->_cmd[1]
+			+ "]\r\n";
+	}
+	else
+		msg += (*it).second;
 	cur_user.add_to_queue(msg);
 	this->_tmp_users[&cur_user] = 0;
 	return (code);
