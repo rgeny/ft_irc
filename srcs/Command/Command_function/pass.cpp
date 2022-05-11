f7afecb60914740c44af97db606d5b7beed35f33
Remy GENY <rgeny@e3r2p10.clusters.42paris.fr>
2022-05-11 13:59:52 +0200

    add paddwdmismatch msg if bad passwd is entry

--- srcs/Command/Command_function/pass.cpp
+++ srcs/Command/Command_function/pass.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 15:13:14 by rgeny             #+#    #+#             */
-/*   Updated: 2022/05/11 13:56:07 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/05/11 13:58:49 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ e_error		Command::_pass	(void)
 	(*this->_users_it)->set_passwd_is_sent(this->_password == this->_cmd[1].c_str());
 	if ((*this->_users_it)->get_passwd_is_sent())
 		return (SUCCESS);
+	this->_err_passwdmismatch();
 	return (ERROR_CONTINUE);
 }
