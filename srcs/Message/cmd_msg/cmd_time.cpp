9faf887a5f9e380f63a637e1d930a8ec8d701b55
abesombe <abesombe@student.42.fr>
2022-05-30 16:31:14 +0200

    change of _hostname par _servername in _cmd_time

--- srcs/Message/cmd_msg/cmd_time.cpp
+++ srcs/Message/cmd_msg/cmd_time.cpp
@@ -6,7 +6,7 @@
 /*   By: ayzapata <ayzapata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 14:03:00 by ayzapata          #+#    #+#             */
-/*   Updated: 2022/05/01 14:34:34 by ayzapata         ###   ########.fr       */
+/*   Updated: 2022/05/30 16:29:17 by ayzapata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 e_error	Message::_cmd_time	(void) const
 {
-	String  msg	= this->_set_msg_base(this->_hostname, "391 "
+	String  msg	= this->_set_msg_base(this->_servername, "391 "
                                     + (*this->_users_it)->get_nickname()
-                                    + " "
-                                    , this->_hostname
+                                    + ""
+                                    , this->_servername
                                     , time(0))
 						            + "\r\n";
 	(*this->_users_it)->add_to_queue(msg);
