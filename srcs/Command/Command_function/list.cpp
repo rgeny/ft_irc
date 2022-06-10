64f86c055a9a249eb507d36d322d733bea27dc55
abesombe <abesombe@student.42.fr>
2022-06-10 15:31:08 +0200

    LIST fixed - works with -YES but also chan_list

--- srcs/Command/Command_function/list.cpp
+++ srcs/Command/Command_function/list.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   list.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abesombes <abesombes@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 15:05:19 by rgeny             #+#    #+#             */
-/*   Updated: 2022/05/06 20:38:28 by abesombe         ###   ########.fr       */
+/*   Updated: 2022/06/10 15:27:13 by abesombes        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,66 @@ e_error	Command::_list	(void)
 	std::vector<String>	list;
 	int nb_users;
 
-	for (CHANS_IT it = _chans.begin(), ite = _chans.end(); it != ite; it++)
+	if (_cmd.size() < 2)
 	{
-		Channel * cur_chan = ((*it).second);
-		String channel_mode;
-		String nb_users_str;
-		if (!cur_chan->get_specific_mode(CHANMODE_p) && !cur_chan->get_specific_mode(CHANMODE_s))
+		for (CHANS_IT it = _chans.begin(), ite = _chans.end(); it != ite; it++)
 		{
-			nb_users = cur_chan->get_chan_user_list().size();
-			channel_mode = "[+"
-			+ String((cur_chan->get_specific_mode(CHANMODE_i) ? "i": ""))
-			+ String((cur_chan->get_specific_mode(CHANMODE_t) ? "t": ""))
-			+ String((cur_chan->get_specific_mode(CHANMODE_n) ? "n": ""))
-			+ String((cur_chan->get_specific_mode(CHANMODE_m) ? "m": ""))
-			+ String((cur_chan->get_specific_mode(CHANMODE_l) ? "l": ""))
-			+ String((cur_chan->get_specific_mode(CHANMODE_b) ? "b": ""))
-			+ String((cur_chan->get_specific_mode(CHANMODE_k) ? "k": ""))
-			+ "]";
-			std::stringstream ss;  
-			ss << nb_users;  
-			ss >> nb_users_str; 
-			list.push_back(cur_chan->get_chan_name() + " " + nb_users_str + " " + channel_mode);
+			Channel * cur_chan = ((*it).second);
+			String channel_mode;
+			String nb_users_str;
+			String topic = cur_chan->get_topic();
+			if (!cur_chan->get_specific_mode(CHANMODE_p) && !cur_chan->get_specific_mode(CHANMODE_s))
+			{
+				nb_users = cur_chan->get_chan_user_list().size();
+				channel_mode = "[+"
+				+ String((cur_chan->get_specific_mode(CHANMODE_i) ? "i": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_t) ? "t": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_n) ? "n": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_m) ? "m": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_l) ? "l": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_b) ? "b": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_k) ? "k": ""))
+				+ "]";
+				std::stringstream ss;  
+				ss << nb_users;  
+				ss >> nb_users_str; 
+				list.push_back(cur_chan->get_chan_name() + " " + nb_users_str + " " + channel_mode + (topic.empty()? "" : " " + topic));
+			}
+		}
+	}
+	else
+	{
+		_chan_list = split(this->_cmd[1], ",");
+		for (std::vector<String>::iterator it = _chan_list.begin(), ite = _chan_list.end(); it != ite; it++)
+		{
+			Channel* cur_chan = (*(_chans.find(*it))).second;
+			String cur_chan_name = *it;
+			if (this->_chan_exist(cur_chan_name) == false)
+			{
+				_cmd[1] = cur_chan_name;
+				_err_nosuchchannel();
+				continue;
+			}
+			String channel_mode;
+			String nb_users_str;
+			String topic = cur_chan->get_topic();
+			if (!cur_chan->get_specific_mode(CHANMODE_p) && !cur_chan->get_specific_mode(CHANMODE_s))
+			{
+				nb_users = cur_chan->get_chan_user_list().size();
+				channel_mode = "[+"
+				+ String((cur_chan->get_specific_mode(CHANMODE_i) ? "i": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_t) ? "t": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_n) ? "n": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_m) ? "m": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_l) ? "l": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_b) ? "b": ""))
+				+ String((cur_chan->get_specific_mode(CHANMODE_k) ? "k": ""))
+				+ "]";
+				std::stringstream ss;  
+				ss << nb_users;  
+				ss >> nb_users_str; 
+				list.push_back(cur_chan->get_chan_name() + " " + nb_users_str + " " + channel_mode + (topic.empty()? "" : " " + topic));
+			}
 		}
 	}
 
