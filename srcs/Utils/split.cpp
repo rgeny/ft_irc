8baef5338fa440caa6e3b042059ad5f5f67482dc
rgeny <remy.geny@gmail.com>
2022-06-18 12:53:53 +0200

    debug channel name (for exemple : #a?)

--- srcs/Utils/split.cpp
+++ srcs/Utils/split.cpp
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 19:32:23 by abesombe          #+#    #+#             */
-/*   Updated: 2022/06/17 07:41:58 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/06/18 12:51:11 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ std::vector<String>	split	(String s
 							,String delimiter)
 {
 	std::vector<String>	result;
+	std::string	tmp(s);
 
-	for (size_t pos = s.find(delimiter); pos != String::npos; pos = s.find(delimiter))
+	for (size_t pos = tmp.find(delimiter); pos != String::npos; pos = tmp.find(delimiter))
 	{
 		if (pos > 0)
 			result.push_back(s.substr(0, pos));
-		s.erase(0, pos + delimiter.length());
+		tmp.erase(0, pos + delimiter.length());
 	}
-	if (!s.empty())
-		result.push_back(s);
+	if (!tmp.empty())
+		result.push_back(tmp);
 	return (result);
 }
 
